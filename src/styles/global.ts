a057cc5b721db02e636e43f9862f16734cb6c23d
henrique <henrique.da.silva5720@gmail.com>
2021-09-09 11:20:05 -0300

    feat: Criado component Button

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,32 +1,30 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+  :root {
+    --white: "#ffffff";
+    --black: "#000000";
+    --gray: "#E5E5E5"
+    --blue-baby: "#0DFFE6";
+    --blue:"#0D17FF";
+    --orange: "#FF9900";
+    --red: "#FF0000";
+  }
 
-:root{
-   --blue-baby: "#0DFFE6";
-   --blue:"#0D17FF";
-   --orange: "#FF9900";
-   --gray: "#E5E5E5"
-   --white: "#ffffff";
-   --black: "#000000";
-   --red: "#FF0000";
-}
-
-*{
+  * {
     margin: 0;
     padding: 0;
     outline: none;
     box-sizing: border-box;
-}
+  }
 
 
-a{
+  a {
     text-decoration: none;
     cursor: pointer;
-}
+  }
 
-button{
+  button {
     cursor: pointer;
-}
-
+  }
 `;
