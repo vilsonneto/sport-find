995e4acfb8b1c5bf639ddbdba38f7b1ece9ddfa5
Miqueias <miqs_carvalho@hotmail.com>
2021-09-13 17:18:20 -0300

    feat: criando um componente q recebe as imagens

--- src/components/CategoryItem/index.tsx
+++ src/components/CategoryItem/index.tsx
@@ -1,35 +1,11 @@
-import basquete from "../../assets/category/basquete.jpg";
-import ciclismo from "../../assets/category/ciclismo.jpg";
-import futebol from "../../assets/category/futebol.jpg";
-import volei from "../../assets/category/volei.jpg";
-import mergulho from "../../assets/category/mergulho.jpg";
-import trilha from "../../assets/category/trilha.jpg";
-import rapel from "../../assets/category/rapel.jpg";
-import skate from "../../assets/category/skate.jpg";
-import surf from "../../assets/category/surf.jpg";
-import yoga from "../../assets/category/yoga.jpg";
-import all from "../../assets/category/all.jpg";
 import Carousel from "../Carousel";
+import { categoryArr } from "../CategoryArr";
 
 interface ICategoryProps {
   filterCategory: (category: string) => void;
 }
 
 const CategoryItem = ({ filterCategory }: ICategoryProps) => {
-  const categoryArr = [
-    { image: all, text: "Todos" },
-    { image: ciclismo, text: "Ciclismo" },
-    { image: volei, text: "Vôlei" },
-    { image: basquete, text: "Basquete" },
-    { image: futebol, text: "Futebol" },
-    { image: yoga, text: "Yoga" },
-    { image: mergulho, text: "Mergulho" },
-    { image: trilha, text: "Trilha" },
-    { image: rapel, text: "Rapel" },
-    { image: skate, text: "Skate" },
-    { image: surf, text: "Surf" },
-  ];
-
   return (
     <Carousel>
       {categoryArr.map((item, index) => (
